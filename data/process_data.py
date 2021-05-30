d49399575cefbaac07184e60640cdc7433c5d7f9
Shimiao Dong <dongshimiao@gmail.com>
2021-05-30 10:40:05 -0600

    fix: A bug fix
    fix to_sql bug. if a table exists, replace it

--- data/process_data.py
+++ data/process_data.py
@@ -68,7 +68,7 @@ def save_data(df, database_filename):
             None
       """
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql(database_filename[5:-3], engine, index=False)
+    df.to_sql(database_filename[5:-3], engine, index=False,if_exists='replace')
  
  
 def main():
